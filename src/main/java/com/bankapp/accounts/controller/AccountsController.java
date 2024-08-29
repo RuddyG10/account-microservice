2af830949039e68113b943fe4db39730436f232f
RuddyG10 <rggt0001@ce.pucmm.edu.do>
2024-08-29 09:37:19 -0400

    agregado

---
+++ src/main/java/com/bankapp/accounts/controller/AccountsController.java
@@ -0,0 +1,13 @@
+package com.bankapp.accounts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountsController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hi World";
+    }
+}
